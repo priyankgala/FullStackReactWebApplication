b496367815a636a031740c824db397be9267b71c
Galapriyank <gala.pr@husky.neu.edu>
2020-08-21 15:12:35 -0400

    Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import { Card, CardBody, CardImg, CardText, CardTitle, CardSubtitle } from 'reactstrap';
 
-function Home(props){
-    return(
-        <div>
-            <h2>Hello, This is my Home Page </h2> 
+function RenderItem({ item }) {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+
+}
+
+function Home(props) {
+    return (
+        <div className="container">
+            <div className="row align-items-start">
+                <div className="col col-md m-1">
+                    <RenderItem item={props.dish} />
+                </div>
+                <div className="col col-md m-1">
+                    <RenderItem item={props.leader} />
+                </div>
+                <div className="col col-md m-1">
+                    <RenderItem item={props.promotion} />
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Menu from './Menu';
-import DishDetailComponent from './DishDetailComponent';
+// import DishDetailComponent from './DishDetailComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactUsComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class App extends Component {
@@ -13,6 +17,9 @@ class App extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
             // selectedDish: null
         };
     }
@@ -26,7 +33,12 @@ class App extends Component {
     render() {
         const Homepage = () => {
             return (
-                <Home />
+                <Home 
+                dish = {this.state.dishes.filter((dish) => dish.featured)[0]}
+                leader = {this.state.leaders.filter((leader) => leader.featured)[0]}
+                promotion = {this.state.promotions.filter((promotion) => promotion.featured)[0]}
+                comment = {this.state.comments.filter((comment) => comment.featured)[0]}
+                />
             );
         }
 
@@ -38,7 +50,9 @@ class App extends Component {
                 <Switch>
                     <Route path="/home" component={Homepage} />
                     <Route path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path = '/contactus' component = {Contact}/>
                     <Redirect to="/home"/>
+
                 </Switch>
                 <Footer />
 
